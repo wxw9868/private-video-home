a292007f626dcf061ae0df218f203e5492d4d659
wxw9868 <wxw9868@163.com>
2024-08-20 20:48:11 +0800

    .

--- model/video.go
+++ model/video.go
@@ -8,11 +8,26 @@ import (
 
 type Video struct {
 	gorm.Model
+	CateID        uint      `gorm:"column:cate_id;type:uint;not null;default:0;comment:视频分类ID"`
 	Title         string    `gorm:"column:title;type:varchar(255);uniqueIndex;comment:标题"`
 	Actress       string    `gorm:"column:actress;type:varchar(100);comment:演员"`
 	Size          int64     `gorm:"column:size;type:bigint;comment:大小"`
 	Duration      float64   `gorm:"column:duration;type:float;default:0;comment:时长"`
 	Poster        string    `gorm:"column:poster;type:varchar(255);comment:封面"`
+	Width         int       `gorm:"column:width;type:int;default:0;comment:宽"`
+	Height        int       `gorm:"column:height;type:int;default:0;comment:高"`
+	CodecName     string    `gorm:"column:codec_name;type:varchar(90);comment:编解码器"`
+	ChannelLayout string    `gorm:"column:channel_layout;type:varchar(90);comment:音频声道"`
+	CreationTime  time.Time `gorm:"column:creation_time;type:date;comment:时间"`
+	Introduction  string    `gorm:"column:introduction;type:text;comment:简介"`
+}
+
+type VideoEpisodes struct {
+	gorm.Model
+	VideoId       uint      `gorm:"column:video_id;type:uint;not null;default:0;comment:视频ID"`
+	Episode       uint      `gorm:"column:episode;type:uint;not null;default:0;comment:剧集"`
+	Size          int64     `gorm:"column:size;type:bigint;comment:大小"`
+	Duration      float64   `gorm:"column:duration;type:float;default:0;comment:时长"`
 	Width         int       `gorm:"column:width;type:int;default:0;comment:宽"`
 	Height        int       `gorm:"column:height;type:int;default:0;comment:高"`
 	CodecName     string    `gorm:"column:codec_name;type:varchar(90);comment:编解码器"`
