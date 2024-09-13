143778753f63ff79633908a8428bfdf064fa042c
v_weixiongwei <v_weixiongwei@weixiongwei-MacBook-Pro.local>
2024-09-13 21:14:14 +0800

    .

--- docs/docs.go
+++ docs/docs.go
@@ -2883,7 +2883,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "192.168.0.9:8080",
+	Host:             "127.0.0.1:8080",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Video API",
